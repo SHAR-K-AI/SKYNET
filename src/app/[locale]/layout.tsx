521e791808ba1036597b62f228536d5e513e2c10
Vadym <vadym.sharai@gmail.com>
2025-09-18 22:15:49 +0300

    wip

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,7 +21,7 @@ export default async function LocaleLayout(
         params: Promise<{ locale: string }>;
     }
 ) {
-    const { locale } = await params as { locale: "en" | "ua" };
+    const { locale } = await params as { locale: "en" | "uk" };
 
     const metaData = {
         en: {
